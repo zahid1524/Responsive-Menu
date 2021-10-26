3edc7bba598a4f3d2d511823b5ff5b77ada90808
unknown <jeszahidhasan@gmail.com>
2021-10-27 00:01:01 +0600

    all files are saved

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".nav-toggler").click(function(){
+        $(".nav").slideToggle();
+      });
+  });
+
+  $(document).ready(function(){
+    $(".nav-toggler").click(function(){
+        $(".open").toggleClass("close");
+      });
+  });
+  
